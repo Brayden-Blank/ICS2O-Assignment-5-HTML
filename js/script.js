67f42584bcef0f8b4638447d77eb20b015a7ac72
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-06-12 14:19:19 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,53 +1,40 @@
-// Copyright (c) 2022 Brayden Blank All rights reserved
-//
-// Created by: Brayden Blank
-// Created on: Apr 2022
-// This file contains the JS functions for index.html
+// Brayden Blank
+// ICS2O-Assignment-5-HTML
+// April 2022
 
-"use strict";
-
+'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit-5-06-HTML/sw.js", {
-    scope: "/ICS2O-Unit-5-06-HTML/",
-  });
+  navigator.serviceWorker.register("/ICS2O-Assignment-5-HTML/sw.js", {
+    scope: "/ICS2O-Assignment-5-HTML/",
+  })
 }
 
-const randomNumber = Math.floor(Math.random() * 6) + 1;
-
 /**
- * This function compares slider with random number.
+ * Finds the value of pi using the user's number of iterations.
  */
-function guessClicked() {
+function onButtonClick() {
+  var numberOfIterations = document.getElementById("number-of-iterations").value
+  var currentAction = "minus"
+  var number = 1
+  var pi
   
-  const counter1 = parseInt(document.getElementById("integer1").value)
-	const counter2 = parseInt(document.getElementById("integer2").value)
-  var addedNumber = 0
-  var result = 0
-
-  if (counter1 > 0 && counter2 > 0) {
-    while (addedNumber < counter2) {
-      addedNumber = addedNumber + 1;
-      result = result + counter1;
+  pi = (4 / number)
+  while (numberOfIterations > 1) {
+    if (currentAction == "minus") {
+      number = number + 2
+      pi = pi - (4 / number)
+      currentAction = "plus"
+    } else if (currentAction == "plus") {
+      number = number + 2
+      pi = pi + (4 / number)
+      currentAction = "minus"
     }
-  } else if (counter1 < 0 && counter2 < 0) {
-    while (addedNumber > counter2) {
-      addedNumber = addedNumber - 1;
-      result = result - counter1;
-    }
-  } else if (counter1 > 0 && counter2 < 0) {
-    while (addedNumber > counter2) {
-      addedNumber = addedNumber - 1;
-      result = result - counter1;
-    }
-  } else {
-    while (addedNumber < counter2) {
-      addedNumber = addedNumber + 1;
-      result = result + counter1;
-    }
+    numberOfIterations = numberOfIterations - 1
   }
-
-document.getElementById('answer').innerHTML = counter1 + " x " + addedNumber + " = " + result;
+  currentAction = "minus"
+  number = 1
+  document.getElementById("result").innerHTML = "<p>π is equal to " + pi + "</p>"
 }
